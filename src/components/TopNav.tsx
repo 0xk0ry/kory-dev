c5f606035c994ad8d455ced584db4a5dbb1c943a
Kory <phamtruc.main@gmail.com>
2024-07-05 06:13:37 +0700

    styles: add background when active

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export function TopNav() {
   const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <nav className="mx-auto mb-6 flex h-auto items-center justify-between border-b p-4 font-semibold md:text-xl">
-      <div className="cursor-pointer"
+      <div
+        className="cursor-pointer"
         onClick={() => {
           router.push("/");
         }}
@@ -13,16 +16,42 @@ export function TopNav() {
         Kory
       </div>
       <div className="flex items-center gap-4">
-        <div className="cursor-pointer"
+        <div
+          className={`cursor-pointer rounded-md px-2 py-[0.1rem] hover:text-slate-600 ${pathname.startsWith("/me") ? "bg-neutral-500" : ""}`}
           onClick={() => {
             router.push("/me");
+            console.log(pathname);
           }}
         >
           me
+        </div>{" "}
+        <div
+          className={`cursor-pointer rounded-md px-2 py-[0.1rem] hover:text-slate-600 ${pathname.startsWith("/blog") ? "bg-neutral-500" : ""}`}
+          onClick={() => {
+            router.push("/blog");
+            console.log(pathname);
+          }}
+        >
+          blog
+        </div>{" "}
+        <div
+          className={`cursor-pointer rounded-md px-2 py-[0.1rem] hover:text-slate-600 ${pathname.startsWith("/others") ? "bg-neutral-500" : ""}`}
+          onClick={() => {
+            router.push("/others");
+            console.log(pathname);
+          }}
+        >
+          others
+        </div>{" "}
+        <div
+          className={`cursor-pointer rounded-md px-2 py-[0.1rem] hover:text-slate-600 ${pathname.startsWith("/github") ? "bg-neutral-500" : ""}`}
+          onClick={() => {
+            router.replace("https://github.com/KoryTheKorgy");
+            console.log(pathname);
+          }}
+        >
+          github
         </div>
-        <div className="cursor-pointer">blog</div>
-        <div className="cursor-pointer">others</div>
-        <div className="cursor-pointer">github</div>
       </div>
     </nav>
   );
